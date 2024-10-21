e2072dbc9c7191268877ff8ae380c6294f7187fd
agonglomarel7 <agonglomarel@gmail.com>
2024-10-22 01:59:19 +0200

    Python refresher

---
+++ Python_Refresher/02_string_formating/code.py
@@ -0,0 +1,17 @@
+
+# two differetns ways to do formatting
+
+# with f
+
+name = "Adi"
+
+gretting = f"Hello,{name}"
+
+print(gretting)
+
+#with .format() function**** with .format() many args can be embbed
+
+
+gretting = "Hello, {}.Today is {}"
+
+print(gretting.format("Adi","tuesday"))
---
+++ Python_Refresher/03_getting_user_input/code.py
@@ -0,0 +1,10 @@
+name = input( "What is yot fisrtname : ")
+
+print(f"My firstname is : {name}")
+
+
+size = input(" What is your house square fit : ")
+
+square_feet = int(size)/10.8
+
+print("{} square feet is {:.2f} square meters".format(size,square_feet))
