3a485855ecc752334974f2f8bf23d6ecdd79e39b
agonglomarel7 <agonglomarel@gmail.com>
2024-10-21 15:15:33 +0200

    Python refresher

---
+++ Python_Refresher/01_variables/code.py
@@ -0,0 +1,32 @@
+
+
+# stock a value in a variable
+
+x = 15 
+
+price = 9.99
+
+discount = 0.2
+
+result = price*(1-discount)
+
+# show the result in the console using print function
+
+print(result)
+
+# store a strings in a variable
+
+name = "Bob"
+
+
+####################### Exercise  ####################################""
+
+
+# Create two variables, var1 and var2, both with the same value.
+
+var1 = 15
+var2 = var1
+
+# Create two variables, num1 and num2, which multiply together to give 16.
+num1=2
+num2=8
