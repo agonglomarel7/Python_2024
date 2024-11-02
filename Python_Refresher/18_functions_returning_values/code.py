7ba1ddae980f5ced636546fbb04f5319083b4145
agonglomarel7 <agonglomarel@gmail.com>
2024-11-02 23:12:47 +0100

    Python refresher

---
+++ Python_Refresher/18_functions_returning_values/code.py
@@ -0,0 +1,8 @@
+#returning value with a function
+
+def sum(a,b):
+
+    return a+b
+
+# return the value 
+print(sum(4,5))
---
+++ Python_Refresher/19_lambda_functions/code.py
@@ -0,0 +1,7 @@
+
+sequence = [1,2,4,7]
+
+#define a lambda function
+lambda x : x*2
+
+doubled = list(map(lambda x : x+5,sequence))
